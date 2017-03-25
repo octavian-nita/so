08b23c0997abffb8f16034b55000d83f297eb764
octavian <octavian.nita@gmail.com>
2017-03-25 15:50:25 +0100

    finished the Vaadin backoffice app

--- so-42927030-vaadin-login-to-site-button/backoffice/src/main/java/com/octaviannita/ui/MainUI.java
+++ so-42927030-vaadin-login-to-site-button/backoffice/src/main/java/com/octaviannita/ui/MainUI.java
@@ -22,8 +22,13 @@ import org.apache.http.message.BasicNameValuePair;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Octavian Theodor NITA (https://github.com/octavian-nita/)
@@ -32,49 +37,78 @@ import java.util.List;
 @Title("Backoffice for SO Question #42927030")
 public class MainUI extends UI {
 
+    private Cookie login(URI targetUri, String loginPath, Map<String, String> params) throws IOException {
+        requireNonNull(targetUri);
+        requireNonNull(loginPath);
+
+        // Keep track of cookies we might receive in an HttpClient:
+        final CookieStore cookies = new BasicCookieStore();
+
+        // Build and work with an (AutoCloseable) HttpClient that uses the cookie store:
+        try (CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(cookies).build()) {
+
+            // Prepare (login) request parameters:
+            List<NameValuePair> reqParams = new ArrayList<>();
+            if (params != null) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
+                    reqParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+                }
+            }
+
+            // Execute the login (POST) request with the given parameters:
+            HttpPost post = new HttpPost(targetUri + loginPath);
+            post.setEntity(new UrlEncodedFormEntity(reqParams));
+
+            CloseableHttpResponse response = client.execute(post);
+            // Eventually, check the response to see if successful
+            response.close();
+
+            // Look for a JSESSIONID-named cookie stored in the HttpClient and return it to be used by calling code:
+            for (org.apache.http.cookie.Cookie cookie : cookies.getCookies()) {
+                if ("JSESSIONID".equalsIgnoreCase(cookie.getName())) {
+
+                    String domain = targetUri.getHost();
+                    if (domain.startsWith("www.")) {
+                        domain = domain.substring(4);
+                    }
+
+                    Cookie authCookie = new Cookie(cookie.getName(), cookie.getValue());
+                    authCookie.setDomain(domain);
+                    authCookie.setPath("/");
+                    // Eventually, set expiry (to allow longer login) and other things...
+
+                    return authCookie;
+                }
+            }
+
+            return null; // some sort of error?
+        }
+    }
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         setContent(new VerticalLayout(new Button("Log into site...", event -> {
 
-            final String site = "http://localhost:8080";
-
-            // Store the cookies we might receive:
-            CookieStore cookies = new BasicCookieStore();
-
-            // Build an HTTP client:
-            CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(cookies).build();
-
-            // Prepare a POST request and log in parameters:
-            HttpPost post = new HttpPost(site + "/log-me-in");
-            List<NameValuePair> params = new ArrayList<>();
-            params.add(new BasicNameValuePair("username", "Johnny"));
-            params.add(new BasicNameValuePair("password", "passwd"));
-
             try {
+                URI targetUri = new URI("http://localhost:8080");
 
-                // Execute the log in (POST) request with the required parameters:
-                post.setEntity(new UrlEncodedFormEntity(params));
-                CloseableHttpResponse response = client.execute(post);
+                Map<String, String> params = new HashMap<>();
+                params.put("username", "Johnny");
+                params.put("password", "incorrect :)");
+                // Eventual hidden fields, etc.
+                // params.put("...", "...");
 
-                // Look for a JSESSIONID-named cookie and add it to the response:
-                for (org.apache.http.cookie.Cookie cookie : cookies.getCookies()) {
-                    if ("JSESSIONID".equalsIgnoreCase(cookie.getName())) {
+                Cookie targetAuthCookie = login(targetUri, "/log-me-in", params);
 
-                        VaadinService.getCurrentResponse().addCookie(new Cookie(cookie.getName(), cookie.getValue()));
-                        Page.getCurrent().getJavaScript().execute("window.open('" + site + "');");
+                // We're not ready just yet: we still need to 'transfer' the cookie
+                // the HTTP client received to the current browser:
+                VaadinService.getCurrentResponse().addCookie(targetAuthCookie);
 
-                        break;
-                    }
-                }
+                // Upon responding to the Vaadin 'click' request, open the target URL (eventually in a new page / tab):
+                Page.getCurrent().getJavaScript().execute("window.open('" + targetUri + "');");
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    client.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
 
         })));
